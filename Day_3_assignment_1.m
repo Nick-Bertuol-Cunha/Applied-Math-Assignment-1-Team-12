63866c54fa62853a1f22552d4308a6f0bc0f386c
HiGoldAnimationStudios <higoldanimationstudios@gmail.com>
2025-09-15 01:58:54 -0400

    day 3 code finished

--- Day_3_assignment_1.m
+++ Day_3_assignment_1.m
@@ -1,4 +1,5 @@
 x_in=linspace(0,50,200);
+x_in_2=linspace(50,0,200);
 [fvalclass, dfdxvalclass]=test_function03(x_in);
 x_guess=27;
 x_guess_2=20;
@@ -8,11 +9,17 @@ newton_success_list=[];
 newton_fail_list=[];
 fzero_success_list=[];
 fzero_fail_list=[];
+secant_success_list=[];
+secant_success_list_2=[];
+secant_fail_list=[];
+secant_fail_list_2=[];
 
 for n=1:length(x_in)
     x_guess=x_in(n);
+    x_guess_2=x_in_2(n);
     newton_root_attempt=newton_solver(@test_function03, x_guess);
     fzero_root_attempt=fzero(@test_function03, x_guess);
+    secant_root_attempt=secant_solver(@test_function03, x_guess, x_guess_2);
     if abs(x_root_3-newton_root_attempt)<.1
         newton_success_list(end+1)=x_guess;
     else
@@ -22,6 +29,13 @@ for n=1:length(x_in)
         fzero_success_list(end+1)=x_guess;
     else
         fzero_fail_list(end+1)=x_guess;
+    end
+    if abs(x_root_3-secant_root_attempt)<.1
+        secant_success_list(end+1)=x_guess;
+        secant_success_list_2(end+1)=x_guess_2;
+    else
+        secant_fail_list(end+1)=x_guess;
+        secant_fail_list_2(end+1)=x_guess_2;
     end
 end
 
@@ -49,6 +63,16 @@ scatter(fzero_success_list, fsuccess_list, 26, 'g', 'filled', 'DisplayName','con
 scatter(fzero_fail_list, ffail_list, 26, 'r', 'filled', 'DisplayName','fails');
 xlabel('initial guess x_0'); ylabel('f(x_0)');
 title('fzero convergence by initial guess (sigmoid)');
+legend('Location','best');
+
+%secant
+figure; hold on;
+plot(x_in, x_in_2, 'k-', 'LineWidth', 1, 'DisplayName','Test Function 3');
+yline(0, '--k', 'LineWidth', 1, 'DisplayName','F(x)=0');
+scatter(secant_success_list, secant_success_list_2, 26, 'g', 'filled', 'DisplayName','converges');
+scatter(secant_fail_list, secant_fail_list_2, 26, 'r', 'filled', 'DisplayName','fails');
+xlabel('initial guess x_0'); ylabel('f(x_0)');
+title('secant convergence by initial guess (sigmoid)');
 legend('Location','best');
 
 function [f_val,dfdx] = test_function03(x)
