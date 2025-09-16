1024153112cdaa3f8abcf057642334b39c4e50e6
Nasa50022 <151699707+Nasa50022@users.noreply.github.com>
2025-09-16 16:31:30 -0400

    waow
    waow

--- Applied_Math_Assignment_1.m
+++ Applied_Math_Assignment_1.m
@@ -1,5 +1,20 @@
-my_recorder = input_recorder();
 
+mmAss1
+function mmAss1()
+%day01();
+%day02();
+fn =(@(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) - 0.7 - exp(x/6));
+flist = [1e-15,1e-2,1e-14,1e-2,2]
+glist1 = linspace(-5, 5, 10);
+glist2 = linspace(-5, 5, 10)+.01;
+convergence_analysis(3,@orion_test_func,0,glist1,glist2,flist)
+
+end
+
+function day01()
+bisection_solver(@(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6),20,40)
+newton_solver(@orion_test_func,30)
+secant_solver(@orion_test_func, 1,3)
 %Day 1
 %Definition of the test function and its derivative
 test_func01 = @(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6);
@@ -18,9 +33,7 @@ fx = test_func01(x);
 %bisection()
 %newton()
 %secant()
-%bisection_solver(@(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6),20,40)
-%newton_solver(@orion_test_func,30)
-%secant_solver(@orion_test_func, 1,3)
+
 function bisection
     test_func01 = @(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6);
     interval = [20,40];
@@ -44,8 +57,6 @@ function bisection
                 % f(b) and f(c) have opposite signs
                 interval = [c,interval(1,2)]
             else
-                % f(b) and f(c) have same signs
-                
             end
 
         end
@@ -76,87 +87,26 @@ function secant()
 end
 
 %generalized bisection function
-function x = bisection_solver(fun,x_left,x_right)
-    func = fun;
-    interval = [x_left,x_right];
-    c_values = [];
 
-    c = (interval(1,1)+interval(1,2))/2;
-    
-    fc = func(c);
-
-    while abs(fc) > 0.000001
-        c = (interval(1,1)+interval(1,2))/2;
-        fa = func(interval(1,1));
-        fb = func(interval(1,2));
-        fc = func(c);
-        
-        c_values = [c_values, c]; % Save the current c value
-
-      
-        % disp(interval)
-
-        if fa * fc < 0
-            % f(a) and f(c) have opposite signs
-            interval = [interval(1,1),c];
-        else
-            % f(a) and f(c) have same signs
-            if fb * fc < 0
-                % f(b) and f(c) have opposite signs
-                interval = [c,interval(1,2)];
-            else
-                % f(b) and f(c) have same signs
-            end
-        end
-    end
-    x = c;
-   % disp('Iterated c values:'); 
-    %disp(c_values);
-end
 
 %generalized newton function
-function [x, xi_values] = newton_solver(fun, x0)
-    tol = 1e-9; max_iter = 100;
-    x = x0; xi_values = [];
-    for n = 1:max_iter
-        [fval, dfdx] = fun(x);          
-        if ~isfinite(fval) || abs(fval) < tol, break; end
-        if ~isfinite(dfdx) || dfdx == 0,  break; end
-        x = x - fval/dfdx;
-        xi_values(end+1) = x;      
-    end
-end
+
 
 
 %generalized secant function
-function x = secant_solver(fun,x0,x1)
-    test_func01 = fun;
-    x1_values = [];
-    while abs(test_func01(x1)) >= 0.00000001
-        fx0 = test_func01(x0);
-        fx1 = test_func01(x1);
-        x2 = (x0 * fx1 - x1 * fx0) / (fx1 - fx0);
-        x1_values = [x1_values, x1];
-        x0 = x1;
-        x1 = x2;
-    end
-    x = x1; 
-    disp('Iterated x1 values:');
-    disp(x1_values);
-end
 
-function [fval,dfdx] = orion_test_func(x)
-    fval =  (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6);
-    dfdx =  3*(x.^2)/100 - 2*x/8 + 2 +(6/2)*cos(x/2+6) - exp(x/6)/6;
-end
 
-function [fval,dfdx] = orion_test_func2(x)
-    fval = x.^2-2;
-    dfdx = 2*x;
-end
 
+
+
+end
 %Day 2
 
+function day02()
+test_func01 = @(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6);
+test_derivative01 = @(x) 3*(x.^2)/100 - 2*x/8 + 2 +(6/2)*cos(x/2+6) - exp(x/6)/6;
+
+my_recorder = input_recorder();
 f_record = my_recorder.generate_recorder_fun(@orion_test_func);
 x0 = 5;
 x_root = newton_solver(f_record,x0);
@@ -209,7 +159,7 @@ for n=1:length(trial_id)
         trial_id(n)>2
         x_regression(end+1) = err_n(n);
         y_regression(end+1) = err_np1(n);
-end
+    end
 end
 
 figure;
@@ -218,6 +168,147 @@ loglog(x_regression,y_regression,'ro','markerfacecolor','r','markersize',1)
 Y = log(y_regression)';
 X1 = log(x_regression)';
 X2 = ones(length(X1),1);
+%run the regression
+
+coeff_vec = regress(Y,[X1,X2]);
+%pull out the coefficients from the fit
+p = coeff_vec(1);
+k = exp(coeff_vec(2));
+
+xx = logspace(log10(min(x_regression)), log10(max(x_regression)), 200);
+
+yy = k .* xx.^p;
+
+figure;                         
+loglog(xx, yy, '-', 'LineWidth', 2);
+
+function [dfdx,d2fdx2] = approximate_derivative(fun,x)
+    delta_x = 1e-6;
+    f_left = fun(x-delta_x);
+    f_0 = fun(x);
+    f_right = fun(x+delta_x);
+    dfdx = (f_right-f_left)/(2*delta_x);
+    d2fdx2 = (f_right-2*f_0+f_left)/(delta_x*delta_x);
+end
+
+f = test_func01;
+
+[fp_fd, fpp_fd] = approximate_derivative(f, x_root);     % finite differences at the root
+p_theory = 2;
+k_theory = abs(0.5 * fpp_fd / fp_fd);
+
+fprintf('Theory:   p = 2, k = %.6g\n', k_theory);
+fprintf('Regression: p = %.4f, k = %.6g\n', p, k);
+end
+
+%STEP 8 TODO
+
+%Example template for analysis function
+%INPUTS:
+%solver_flag: an integer from 1-4 indicating which solver to use
+% 1->Bisection 2-> Newton 3->Secant 4->fzero
+%fun: the mathematical function that we are using the
+% solver to compute the root of
+%x_guess0: the initial guess used to compute x_root
+%guess_list1: a list of initial guesses for each trial
+%guess_list2: a second list of initial guesses for each trial
+% if guess_list2 is not needed, then set to zero in input
+%filter_list: a list of constants used to filter the collected data
+function convergence_analysis(solver_flag, fun, x_guess0, guess_list1, guess_list2, filter_list)
+
+
+test_func01 = fun;
+test_derivative01 = @(x) approximate_derivative(fun, x);
+
+my_recorder = input_recorder();
+f_record = my_recorder.generate_recorder_fun(test_func01);
+x0 = x_guess0;
+
+       x_root = newton_solver(f_record,x0);
+      
+    
+input_list = my_recorder.get_input_list();
+%semilogy(1:length(input_list),abs(input_list-x_root),'ko','markerfacecolor','k');
+
+xn    = input_list(1:end-1);
+xnp1  = input_list(2:end);
+nVals = (1:numel(xn)).';   % iteration index for each pair
+
+num_trials = numel(guess_list1);
+
+x0_list    = guess_list1;
+
+xn_all = []; xnp1_all = []; n_all = []; trial_id = [];
+
+for t = 1:numel(x0_list)
+    rec = input_recorder();
+    f_rec = rec.generate_recorder_fun(test_func01); 
+    try
+        for i = 1:num_trials
+            if solver_flag == 1 % Bisection
+                x_star = bisection_solver(f_rec,x0_list(t),guess_list2(t));
+
+            elseif solver_flag == 2 % Newton
+                x_star = newton_solver(f_rec,x0_list(t));
+            
+            elseif solver_flag == 3 % Secant
+                x_star = secant_solver(f_rec,x0_list(t),guess_list2(t));
+            elseif solver_flag == 4 % fzero
+                x_star = fzero(f_rec,x0_list(t));
+            
+            end
+            i = i+1;
+    end
+          
+    catch
+        continue
+    end
+
+    xs = rec.get_input_list();                          
+    if numel(xs) < 2, continue; end
+
+    xn   = xs(1:end-1).';
+    xnp1 = xs(2:end).';
+    n    = (1:numel(xn)).';
+    xn_all    = [xn_all;    xn];
+    xnp1_all  = [xnp1_all;  xnp1];
+    n_all     = [n_all;     n];
+    trial_id  = [trial_id;  t*ones(numel(n),1)];
+t = t+1;
+end
+
+fprintf('Collected %d pairs from %d trials (target root ~ %.12g)\n', ...
+    numel(xn_all), numel(unique(trial_id)), x_root);
+
+err_n=abs(xn_all-x_root);
+err_np1=abs(xnp1_all-x_root);
+figure;
+loglog(err_n,err_np1,'ro','markerfacecolor','r','markersize',1)
+
+size(err_n)
+size(err_np1)
+
+x_regression=[];
+y_regression=[];
+for n=1:length(trial_id)
+    if err_n(n)>filter_list(1) && err_n(n)<filter_list(2) && ...
+        err_np1(n)>filter_list(3) && err_np1(n)<filter_list(4) && ...
+        trial_id(n)>filter_list(5)
+        x_regression(end+1) = err_n(n);
+        y_regression(end+1) = err_np1(n);
+    end
+    n=n+1;
+end
+
+size(x_regression)
+size(y_regression)
+figure;
+loglog(x_regression,y_regression,'ro','markerfacecolor','r','markersize',1)
+
+Y = log(y_regression)';
+X1 = log(x_regression)';
+X2 = ones(length(X1),1);
+
 %run the regression
 coeff_vec = regress(Y,[X1,X2]);
 %pull out the coefficients from the fit
@@ -248,54 +339,6 @@ k_theory = abs(0.5 * fpp_fd / fp_fd);
 
 fprintf('Theory:   p = 2, k = %.6g\n', k_theory);
 fprintf('Regression: p = %.4f, k = %.6g\n', p, k);
-
-
-%STEP 8 TODO
-
-%Example template for analysis function
-%INPUTS:
-%solver_flag: an integer from 1-4 indicating which solver to use
-% 1->Bisection 2-> Newton 3->Secant 4->fzero
-%fun: the mathematical function that we are using the
-% solver to compute the root of
-%x_guess0: the initial guess used to compute x_root
-%guess_list1: a list of initial guesses for each trial
-%guess_list2: a second list of initial guesses for each trial
-% if guess_list2 is not needed, then set to zero in input
-%filter_list: a list of constants used to filter the collected data
-
-guess_list_1
-
-convergence_analysis(2,test_func01, 3)
-
-function convergence_analysis(solver_flag, fun, x_guess0, guess_list1, guess_list2, filter_list)
-    % Initialize variables
-    num_trials = length(guess_list1);
-    roots = zeros(num_trials, 1);
-    iterations = zeros(num_trials, 1);
-    
-    % Loop through each guess in guess_list1
-    for i = 1:num_trials
-        if solver_flag == 1 % Bisection
-            roots(i) = bisection_solver(fun, guess_list1(i), guess_list2(i));
-        elseif solver_flag == 2 % Newton
-            [roots(i), xi_values] = newton_solver(fun, guess_list1(i));
-            iterations(i) = length(xi_values);
-        elseif solver_flag == 3 % Secant
-            roots(i) = secant_solver(fun, guess_list1(i), guess_list2(i));
-        elseif solver_flag == 4 % fzero
-            roots(i) = fzero(fun, guess_list1(i));
-        end
-    end
-    
-    % Filter results based on filter_list
-    filtered_roots = roots(ismember(roots, filter_list));
-    
-    % Display results
-    fprintf('Roots found:\n');
-    disp(filtered_roots);
-    fprintf('Number of iterations for each trial:\n');
-    disp(iterations);
 end
 
 %day 3
